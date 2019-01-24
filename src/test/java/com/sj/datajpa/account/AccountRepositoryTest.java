6a797b05c591b08d9fc6be4b6688e455e3515de1
sangjin <sangjin0309@github.com>
2019-01-24 21:48:06 +0900

    commit

---
+++ src/test/java/com/sj/datajpa/account/AccountRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.sj.datajpa.account;
+
+import org.assertj.core.api.OptionalAssert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class AccountRepositoryTest {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    public void di() throws SQLException {
+        Account account = new Account();
+        account.setUsername("sangjin");
+        account.setPassword("123456");
+
+        Account newAccount = accountRepository.save(account);
+
+        assertThat(newAccount).isNotNull();
+
+        Optional<Account> existingAccount = accountRepository.findByUsername(newAccount.getUsername());
+        assertThat(existingAccount).isNotEmpty();
+
+        Optional<Account> nonExistingAccount = accountRepository.findByUsername("sangjin");
+        assertThat(nonExistingAccount).isEmpty();
+    }
+
+}
